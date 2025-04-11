438a857e4c92d1124063982070f94b0bbdb9d20c
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-11 14:39:29 +0300

    Added controller and repository classes.

---
+++ src/main/java/app/web/IndexController.java
@@ -0,0 +1,23 @@
+package app.web;
+
+import app.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class IndexController {
+
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+}
