2f2b31078d73a032867b85effbbfdded98db9554
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-11 14:37:25 +0300

    Custom Exception.

---
+++ src/main/java/app/exceptions/UsernameNotFoundException.java
@@ -0,0 +1,7 @@
+package app.exceptions;
+
+public class UsernameNotFoundException extends RuntimeException {
+    public UsernameNotFoundException(String message) {
+        super(message);
+    }
+}
