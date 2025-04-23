920dc5ddd5ac7311d1d4df23725afb35fa19b46d
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-24 01:13:55 +0300

    Create ExceptionHandlerAdvice.java

---
+++ src/main/java/app/web/dto/ExceptionHandlerAdvice.java
@@ -0,0 +1,39 @@
+package app.web.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({
+            AccessDeniedException.class,
+            NoResourceFoundException.class,
+            MethodArgumentTypeMismatchException.class,
+            MissingRequestValueException.class
+    })
+    public ModelAndView handleNotFoundExceptions() {
+
+        return new ModelAndView("not-found");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleAnyException(Exception exception) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("server-error");
+        modelAndView.addObject("errorMessage", exception.getClass().getSimpleName());
+
+        return modelAndView;
+    }
+}
