5cd667f0a98121e5524ec21d46a11d6ecd8f1b0d
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-29 16:09:05 +0300

    Added links to nav bar.

---
+++ src/main/java/app/web/UserController.java
@@ -0,0 +1,35 @@
+package app.web;
+
+import app.security.UserAuthDetails;
+import app.user.model.User;
+import app.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public ModelAndView getUserProfilePage(@AuthenticationPrincipal UserAuthDetails userAuthDetails) {
+
+        User user = userService.getUserById(userAuthDetails.getId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("profile");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+}
